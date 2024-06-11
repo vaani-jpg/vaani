4a2717b80c8880f758a0062fe78fac24ff286715
vaani-jpg <dayd9327@gmail.com>
2024-06-12 00:27:57 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0;
+    do
+    {
+        printf("hello world\n");
+        i++;
+        }
+        while(i<=15);
+        return 0;
+        }
+    
